e314fe2d94f500d879f26bc2b047ccd40c959547
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-10-16 19:52:04 +0100

    Replacing class components with functional components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Components/HeaderComponent/header-component';
 import CardList from './Components/CardListComponent/cardlist-component';
-class App extends Component{
+
+
+
+const App = () =>{
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(Response => Response.json())
+    .then(users => { setMonsters(users) })
+  },[])
+
+  useEffect(() =>{
+    const newFilteredMonsters = monsters.filter((filtered) => filtered.name.toLowerCase().includes(searchField.toLowerCase()))
+    setFilteredMonsters(newFilteredMonsters)
+  },[monsters, searchField])
+  
+  const searchChange = (e) =>{
+    let searchString = e.target.value.toLowerCase()
+    setSearchField(searchString)
+  }
+
+  return(
+      <div className="App">
+        <Header search={searchChange}/>
+        <CardList monsters={filteredMonsters}/>
+      </div>
+  ); 
+}
+/**class App extends Component{
   constructor(){
     super();
 
@@ -36,5 +67,5 @@ class App extends Component{
     );
   }
 }
-
+**/
 export default App;
